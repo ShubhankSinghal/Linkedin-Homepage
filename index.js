51da88001dec8bcdc261e510b04035c35957af51
ShubhankSinghal <shubhank.singhal98@gmail.com>
2020-07-03 23:52:36 +0530

    left part

--- index.js
+++ index.js
@@ -6,3 +6,36 @@ function toggle1() {
       x.style.display = "none";
     }
   }
+
+  function toggle2() {
+    var x = document.getElementById("recent");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function toggle3() {
+    var x = document.getElementById("follow");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+//event form
+function myBtn() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closed() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == document.getElementById("myModal")) {
+    document.getElementById("myModal").style.display = "none";
+  }
+}
